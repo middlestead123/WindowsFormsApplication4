908f5b69a6a6b7673ea6dccef266abab3d501336
abbey <middlestead1234@gmail.com>
2017-01-26 15:40:48 -0600

    final project
    final project for computer science

---
+++ WindowsFormsApplication4/FinalProject.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class frmFinalProject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnButton1 = new System.Windows.Forms.Button();
+            this.lblInputandOutput = new System.Windows.Forms.Label();
+            this.btnButton2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnButton3 = new System.Windows.Forms.Button();
+            this.lblStringFormat = new System.Windows.Forms.Label();
+            this.lblButton4 = new System.Windows.Forms.Button();
+            this.lblCommenting = new System.Windows.Forms.Label();
+            this.btnButton5 = new System.Windows.Forms.Button();
+            this.lblArrays = new System.Windows.Forms.Label();
+            this.btnButton6 = new System.Windows.Forms.Button();
+            this.lblMethods = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnButton1
+            // 
+            this.btnButton1.Location = new System.Drawing.Point(1, -1);
+            this.btnButton1.Name = "btnButton1";
+            this.btnButton1.Size = new System.Drawing.Size(76, 23);
+            this.btnButton1.TabIndex = 0;
+            this.btnButton1.Text = "Button 1";
+            this.btnButton1.UseVisualStyleBackColor = true;
+            this.btnButton1.Click += new System.EventHandler(this.btnButton1_Click);
+            // 
+            // lblInputandOutput
+            // 
+            this.lblInputandOutput.Location = new System.Drawing.Point(-1, 25);
+            this.lblInputandOutput.Name = "lblInputandOutput";
+            this.lblInputandOutput.Size = new System.Drawing.Size(141, 23);
+            this.lblInputandOutput.TabIndex = 1;
+            this.lblInputandOutput.Click += new System.EventHandler(this.lblInputandOutput_Click);
+            // 
+            // btnButton2
+            // 
+            this.btnButton2.Location = new System.Drawing.Point(2, 66);
+            this.btnButton2.Name = "btnButton2";
+            this.btnButton2.Size = new System.Drawing.Size(104, 23);
+            this.btnButton2.TabIndex = 2;
+            this.btnButton2.Text = "Button 2";
+            this.btnButton2.UseVisualStyleBackColor = true;
+            this.btnButton2.Click += new System.EventHandler(this.btnSecondButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(2, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnButton3
+            // 
+            this.btnButton3.Location = new System.Drawing.Point(1, 143);
+            this.btnButton3.Name = "btnButton3";
+            this.btnButton3.Size = new System.Drawing.Size(75, 23);
+            this.btnButton3.TabIndex = 4;
+            this.btnButton3.Text = "Button 3";
+            this.btnButton3.UseVisualStyleBackColor = true;
+            this.btnButton3.Click += new System.EventHandler(this.btnButton3_Click);
+            // 
+            // lblStringFormat
+            // 
+            this.lblStringFormat.Location = new System.Drawing.Point(2, 178);
+            this.lblStringFormat.Name = "lblStringFormat";
+            this.lblStringFormat.Size = new System.Drawing.Size(89, 34);
+            this.lblStringFormat.TabIndex = 5;
+            this.lblStringFormat.Click += new System.EventHandler(this.lblStringFormat_Click);
+            // 
+            // lblButton4
+            // 
+            this.lblButton4.Location = new System.Drawing.Point(1, 278);
+            this.lblButton4.Name = "lblButton4";
+            this.lblButton4.Size = new System.Drawing.Size(90, 23);
+            this.lblButton4.TabIndex = 6;
+            this.lblButton4.Text = "Button 4";
+            this.lblButton4.UseVisualStyleBackColor = true;
+            // 
+            // lblCommenting
+            // 
+            this.lblCommenting.Location = new System.Drawing.Point(2, 317);
+            this.lblCommenting.Name = "lblCommenting";
+            this.lblCommenting.Size = new System.Drawing.Size(128, 50);
+            this.lblCommenting.TabIndex = 7;
+            this.lblCommenting.Click += new System.EventHandler(this.lblCommenting_Click);
+            // 
+            // btnButton5
+            // 
+            this.btnButton5.Location = new System.Drawing.Point(1, 395);
+            this.btnButton5.Name = "btnButton5";
+            this.btnButton5.Size = new System.Drawing.Size(75, 23);
+            this.btnButton5.TabIndex = 8;
+            this.btnButton5.Text = "Button 5";
+            this.btnButton5.UseVisualStyleBackColor = true;
+            // 
+            // lblArrays
+            // 
+            this.lblArrays.Location = new System.Drawing.Point(12, 437);
+            this.lblArrays.Name = "lblArrays";
+            this.lblArrays.Size = new System.Drawing.Size(94, 65);
+            this.lblArrays.TabIndex = 9;
+            // 
+            // btnButton6
+            // 
+            this.btnButton6.Location = new System.Drawing.Point(292, -1);
+            this.btnButton6.Name = "btnButton6";
+            this.btnButton6.Size = new System.Drawing.Size(75, 23);
+            this.btnButton6.TabIndex = 10;
+            this.btnButton6.Text = "Button 6";
+            this.btnButton6.UseVisualStyleBackColor = true;
+            // 
+            // lblMethods
+            // 
+            this.lblMethods.AutoSize = true;
+            this.lblMethods.Location = new System.Drawing.Point(303, 25);
+            this.lblMethods.Name = "lblMethods";
+            this.lblMethods.Size = new System.Drawing.Size(0, 13);
+            this.lblMethods.TabIndex = 11;
+            // 
+            // frmFinalProject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 511);
+            this.Controls.Add(this.lblMethods);
+            this.Controls.Add(this.btnButton6);
+            this.Controls.Add(this.lblArrays);
+            this.Controls.Add(this.btnButton5);
+            this.Controls.Add(this.lblCommenting);
+            this.Controls.Add(this.lblButton4);
+            this.Controls.Add(this.lblStringFormat);
+            this.Controls.Add(this.btnButton3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnButton2);
+            this.Controls.Add(this.lblInputandOutput);
+            this.Controls.Add(this.btnButton1);
+            this.Name = "frmFinalProject";
+            this.Text = "Final Project";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnButton1;
+        private System.Windows.Forms.Label lblInputandOutput;
+        private System.Windows.Forms.Button btnButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnButton3;
+        private System.Windows.Forms.Label lblStringFormat;
+        private System.Windows.Forms.Button lblButton4;
+        private System.Windows.Forms.Label lblCommenting;
+        private System.Windows.Forms.Button btnButton5;
+        private System.Windows.Forms.Label lblArrays;
+        private System.Windows.Forms.Button btnButton6;
+        private System.Windows.Forms.Label lblMethods;
+    }
+}
+
---
+++ WindowsFormsApplication4/FinalProject.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class frmFinalProject : Form
+    {
+        public frmFinalProject()
+        {
+            InitializeComponent();
+        }
+
+        private void lblInputandOutput_Click(object sender, EventArgs e)
+        {
+            /*Dealing with variables
+             * 
+             */
+
+            lblResult.display = ("yay you clicked on the button");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            /*Dealing with string.format
+          * 
+          */
+
+            const double PST_RATE = 0.08;
+            const double GST_RATE = 0.05;
+
+            double coffeePrice = 1.80;
+            int numCoffees;
+            double subTotal;
+            double taxes;
+            double finalTotal;
+
+            numCoffees = Convert.ToInt32(txtCoffeeImput.Text);
+            subTotal = numCoffees * coffeePrice;
+            taxes = subTotal = (PST_RATE + GST_RATE);
+            finalTotal = subTotal + taxes;
+
+            string mystring = "zeppelin";
+
+            lblDisplay.Text = (mystring.Length).ToString();
+            lblDisplay.Text += "\n" + mystring.Substring(4);
+            lblDisplay.Text += "\n" + mystring.Substring(3, 1);
+            lblDisplay.Text += "\n" + mystring.IndexOf('1');
+            lblDisplay.Text += "\n\n PST is" + PST_RATE;
+            lblDisplay.Text += "\n" + "GST is" + GST_RATE;
+            lblDisplay.Text += "\n" + "coffee costs $" + coffeePrice;
+            lblDisplay.Text += "\n" + "your total is $" + finalTotal;
+        }
+
+        private void btnSecondButton_Click(object sender, EventArgs e)
+        {
+            /*
+             * In this section we are dealing with methods
+             */
+
+            displayName();
+
+            displayBirthday();
+
+            displayFact();
+
+        }
+
+        public static void displayName()
+        {
+
+            System.out.println("my name is abbey");
+
+        }
+
+        public static void displayBirthday()
+        {
+
+            System.out.println("09/08/1999");
+        }
+
+        public static void displayFact()
+        {
+
+            System.out.println("i like writing books");
+        }
+
+        private void btnButton1_Click(object sender, EventArgs e)
+        {
+            /**
+             * In this section we are dealing with arrays
+             */
+
+            int[] Myarray = { 13, 17, 20, 2, 6, 8, -3, 10 };
+            int index = -1;
+            int minimum;
+            int userValue;
+
+            //to iterate through an array, we use a for loop!
+            // i is the current index we are looking at in the array
+            // i will continue until it is as large as the length 
+            // of the array 
+            for (int i = 0; i < Myarray.length; i++)
+            {
+
+                //this will display all values 
+                System.out.println(Myarray[i] + "   ");
+            }
+            //this code gets the value you want to search from the user
+            System.out.println();
+            System.out.println("what vlaue do you want the index of?");
+            userValue = userInput.nextInt();
+
+            //to search for something you have to look at every element 
+            //of the array. this is accomplished by looping through
+            //all elements
+            for (int i = 0; i < Myarray.length; i++)
+            {
+                if (Myarray[i] == userValue)
+                {
+                    index = i;
+                }
+            }
+
+            if (index == -1)
+            {
+                System.out.println("this is not in the array!");
+            }
+            else
+            {
+                System.out.println(userValue + " is at index " + index);
+            }
+
+        }   /**
+		* Finding the minimum value of the array
+		*/
+
+        minimum = Myarray[0];
+	
+	//to find the minimum value of the array, we have to
+	//search through every element of the array and see if
+	//the current value is smaller than the previous value
+	
+	for(int i = 0; i<Myarray.length; i++){
+		
+		//checks whether the current element is less then minimum
+		if(Myarray[i] < minimum){
+			
+			//if yes, change minimum to the current element
+			minimum = Myarray[i];
+				
+		}
+
+}
+system.out.println("the minimum value is: " + minimum);
+        }
+
+        private void btnButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStringFormat_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblCommenting_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
